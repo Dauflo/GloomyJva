482f454f7fb2480141f01e531eb1e3a9cf2e32bb
Dauflo <284994@supinfo.com>
2018-05-04 17:18:14 +0200

    Update main page JSP and Servlet

--- src/com/gloomy/servlet/Main.java
+++ src/com/gloomy/servlet/Main.java
@@ -1,25 +1,36 @@
 package com.gloomy.servlet;
 
+import com.gloomy.dao.FileDao;
+import com.gloomy.entity.FileUser;
+import com.gloomy.entity.User;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = Main.URL_PATH)
 public class Main extends HttpServlet {
     public static final String URL_PATH = "/auth/main";
     public static final String JSP_PATH = "/WEB-INF/auth/main.jsp";
+    private FileDao fileDao;
 
     @Override
     public void init() throws ServletException {
-
+        fileDao = new FileDao();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User currentUser = (User) session.getAttribute("user");
+        List<FileUser> fileUserList = fileDao.getFileUserList(currentUser);
+        req.setAttribute("files", fileUserList);
         RequestDispatcher rd = req.getRequestDispatcher(Main.JSP_PATH);
         rd.forward(req, resp);
     }
--- web/WEB-INF/auth/main.jsp
+++ web/WEB-INF/auth/main.jsp
@@ -6,15 +6,36 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
     <title>Gloomy</title>
 </head>
 <body>
 <div>
-    Main page
+    Main page<br><br>
+    <form method="post" action="/auth/uploadfile" enctype="multipart/form-data">
+        My file<br>
+        <input type="file" name="file" size="50">
+        <br><br>
+        <input type="submit" value="Upload">
+    </form>
     <a href="/logout">Logout</a>
-    <a href="/auth/updateprofil">Update Profil</a>
+    <a href="/auth/updateprofil">Update Profil</a><br>
+    <table>
+        <c:forEach items="${files}" var="file">
+            <tr>
+                <td><c:out value="${file.getId()}"/></td>
+                <td><c:out value="${file.getName()}"/></td>
+                <td>
+                    <form method="get" action="/auth/downloadfile">
+                        <input type="hidden" value="${file.getId()}" name="id"/>
+                        <input type="submit" value="Get file"/>
+                    </form>
+                </td>
+            </tr>
+        </c:forEach>
+    </table>
 </div>
 </body>
 </html>
