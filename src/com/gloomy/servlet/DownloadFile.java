27f5291d194e65d1e74ab09554e983f8a5ac35ad
Dauflo <284994@supinfo.com>
2018-05-06 16:51:58 +0200

    Remove useless code

--- src/com/gloomy/servlet/DownloadFile.java
+++ src/com/gloomy/servlet/DownloadFile.java
@@ -21,11 +21,6 @@ public class DownloadFile extends HttpServlet {
     public static final String URL_PATH = "/auth/downloadfile";
     private FileDao fileDao;
 
-    private final String JDDC_DRIVER = "com.mysql.jdbc.Driver";
-    private final String DB_URL = "jdbc:mysql://localhost/gloomydb";
-    private final String USER = "root";
-    private final String PASS = "";
-
     @Override
     public void init() throws ServletException {
         fileDao = new FileDao();
@@ -82,47 +77,6 @@ public class DownloadFile extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        /*long id = 1;
-        FileUser fileUser = null;
-        InputStream is = null;
-        try {
-            fileUser = fileDao.read(id);
-
-
-            String fileName = fileUser.getName();
-            System.out.println("File Name: " + fileName);
-
-            // abc.txt => text/plain
-            // abc.zip => application/zip
-            // abc.pdf => application/pdf
-            String contentType = this.getServletContext().getMimeType(fileName);
-            System.out.println("Content Type: " + contentType);
-
-            resp.setHeader("Content-Type", contentType);
-
-            resp.setHeader("Content-Length", String.valueOf(fileUser.getFilePart().length()));
-
-            resp.setHeader("Content-Disposition", "inline; filename=\"" + fileUser.getName() + "\"");
-
-            // For big BLOB data.
-            Blob fileData = fileUser.getFilePart();
-
-            is = fileData.getBinaryStream();
-            byte[] bytes = new byte[BUFFER_SIZE];
-            int bytesRead = -1;
-
-            while ((bytesRead = is.read(bytes)) != -1) {
-                // Write image data to Response.
-                resp.getOutputStream().write(bytes, 0, bytesRead);
-            }
-
-            is.close();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        resp.sendRedirect(Main.URL_PATH);*/
-
 
     }
 }
